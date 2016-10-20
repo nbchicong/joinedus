8782ff005517db7684f26381c9cd233afa700ce9
Nguyễn Bá Chí Công <nbccong@inetcloud.vn>
2016-10-20 18:33:08 +0700

    remove model

--- app/FileEntry.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-class FileEntry extends BaseModel {
-  protected $primaryKey = 'id';
-  protected $table = 'file_store';
-  protected $fillable = array('code', 'filename', 'mimetype', 'original_filename');
-}
