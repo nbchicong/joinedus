c15697ae8dfbff3884006688e39d229c22472879
Nguyễn Bá Chí Công <nbccong@inetcloud.vn>
2016-10-20 18:45:05 +0700

    Web constant

---
+++ app/WebConstant.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) 2016 CT1905
+ * Created by Nguyen Ba Chi Cong <nbchicong@gmail.com>
+ * Date: 27/08/2016
+ * Time: 6:30 AM
+ * ---------------------------------------------------
+ * Project: laravel
+ * @name: WebConstant.php
+ * @package: App
+ * @author: nbchicong
+ */
+/**
+ * Class WebConstant
+ * @package App
+ */
+
+
+namespace App;
+
+class WebConstant {
+  const PAGE_LIMIT_PARAM = "pageSize";
+  const PAGE_OFFSET_PARAM = "pageNumber";
+  const LANG_PARAM = "intl";
+  const PREFIX_SET_FUNC_MODEL = "set";
+  const PREFIX_GET_FUNC_MODEL = "get";
+  const WEB_EXT = ".html";
+  const SERVICE_EXT = ".svc";
+}
