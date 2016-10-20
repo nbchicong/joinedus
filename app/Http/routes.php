38bf1ca478f829f79c4433e1c72c7544227faf33
Nguyễn Bá Chí Công <nbccong@inetcloud.vn>
2016-10-20 17:55:35 +0700

    update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,13 @@
 |
 */
 
+// WEB CBL
+Route::get('/', 'Web\CBLIndexController@index');
+Route::get('/index.html', 'Web\CBLIndexController@index');
+
 // WEB E-SHOPPER
-Route::get('/','Web\HomeController@index');
-Route::get('/index', 'Web\HomeController@index');
+//Route::get('/','Web\HomeController@index');
+Route::get('/home', 'Web\HomeController@index');
 Route::get('/products','Web\HomeController@products');
 Route::get('/products/details/{code}','Web\HomeController@productDetails');
 Route::get('/products/categories/{code}','Web\HomeController@productCategories');
@@ -21,14 +25,18 @@ Route::get('/products/brands/{brandCode}','Web\HomeController@productBrands');
 Route::get('/blog','Web\HomeController@blog');
 Route::get('/blog/entry/{code}','Web\HomeController@blogItem');
 Route::get('/contact-us','Web\HomeController@contactUs');
-Route::get('/login','Web\HomeController@login');
-Route::get('/logout','Web\HomeController@logout');
 Route::get('/cart','Web\HomeController@cart');
 Route::get('/checkout','Web\HomeController@checkout');
 Route::get('/search/{query}','Web\HomeController@search');
 
+Route::get('/login','Auth\AuthController@loginForm');
+//Route::get('/logout','Auth\AuthController@logoutAction');
+Route::get('/logout','Web\HomeController@logout');
 Route::post('/auth/login','Auth\AuthController@login');
-Route::post('/auth/register','Auth\AuthController@create');
+Route::post('/register','Web\RegisterController@register');
+
+Route::get('/test','TestController@getSomething');
+Route::get('/test/{color}/{name}','TestController@getByUrl');
 
 // ADMIN
 
@@ -42,27 +50,27 @@ Route::group(['middleware' => ['roles:writer']], function() {
   Route::post('/admin/file/add','Admin\FileEntryController@add');
   Route::get('/admin/file/list','Admin\FileEntryController@listFile');
 
-  Route::get('/admin/product','Admin\ProductController@index');
+  Route::get(Helper::getRouteWebPath('/admin/product'),'Admin\ProductController@index');
   Route::get('/admin/product/list','Admin\ProductController@listItems');
   Route::get('/admin/product/load','Admin\ProductController@load');
   Route::post('/admin/product/create','Admin\ProductController@create');
   Route::post('/admin/product/update','Admin\ProductController@update');
   Route::post('/admin/product/remove','Admin\ProductController@delete');
 
-  Route::get('/admin/entry/category','Admin\EntryCategoryController@index');
+  Route::get(Helper::getRouteWebPath('/admin/entry/category'),'Admin\EntryCategoryController@index');
   Route::get('/admin/entry/category/list','Admin\EntryCategoryController@listItems');
   Route::post('/admin/entry/category/create','Admin\EntryCategoryController@create');
   Route::post('/admin/entry/category/update','Admin\EntryCategoryController@update');
   Route::post('/admin/entry/category/remove','Admin\EntryCategoryController@delete');
 
-  Route::get('/admin/entry','Admin\EntryController@index');
+  Route::get(Helper::getRouteWebPath('/admin/entry'),'Admin\EntryController@index');
   Route::get('/admin/entry/list','Admin\EntryController@listItems');
   Route::get('/admin/entry/load','Admin\EntryController@load');
   Route::post('/admin/entry/create','Admin\EntryController@create');
   Route::post('/admin/entry/update','Admin\EntryController@update');
   Route::post('/admin/entry/remove','Admin\EntryController@delete');
 
-  Route::get('/admin/page','Admin\PageContentController@index');
+  Route::get(Helper::getRouteWebPath('/admin/page'),'Admin\PageContentController@index');
   Route::get('/admin/page/list','Admin\PageContentController@listItems');
   Route::get('/admin/page/load','Admin\PageContentController@load');
   Route::post('/admin/page/create','Admin\PageContentController@create');
@@ -70,30 +78,31 @@ Route::group(['middleware' => ['roles:writer']], function() {
   Route::post('/admin/page/remove','Admin\PageContentController@delete');
 });
 Route::group(['middleware' => ['roles:admin']], function() {
-  Route::get('/admin/product/category','Admin\ProductCategoryController@index');
+  Route::get(Helper::getRouteWebPath('/admin/product/category'),'Admin\ProductCategoryController@index');
   Route::get('/admin/product/category/list','Admin\ProductCategoryController@listCate');
   Route::post('/admin/product/category/create','Admin\ProductCategoryController@create');
   Route::post('/admin/product/category/update','Admin\ProductCategoryController@update');
   Route::post('/admin/product/category/remove','Admin\ProductCategoryController@delete');
 
-  Route::get('/admin/product/brand','Admin\BrandController@index');
+  Route::get(Helper::getRouteWebPath('/admin/product/brand'),'Admin\BrandController@index');
   Route::get('/admin/product/brand/list','Admin\BrandController@listCate');
   Route::post('/admin/product/brand/create','Admin\BrandController@create');
   Route::post('/admin/product/brand/update','Admin\BrandController@update');
   Route::post('/admin/product/brand/remove','Admin\BrandController@delete');
 
-  Route::get('/admin/carousel','Admin\CarouselController@index');
+  Route::get(Helper::getRouteWebPath('/admin/carousel'),'Admin\CarouselController@index');
   Route::get('/admin/carousel/list','Admin\CarouselController@listItems');
   Route::get('/admin/carousel/load','Admin\CarouselController@load');
   Route::post('/admin/carousel/create','Admin\CarouselController@create');
   Route::post('/admin/carousel/update','Admin\CarouselController@update');
   Route::post('/admin/carousel/remove','Admin\CarouselController@delete');
 
-  Route::get('/admin/config','Admin\SiteConfigController@index');
+  Route::get(Helper::getRouteWebPath('/admin/config'),'Admin\SiteConfigController@index');
   Route::post('/admin/config/update','Admin\SiteConfigController@update');
 });
+
 Route::group(['middleware' => ['roles:super_admin']], function() {
-  Route::get('/admin/users','Admin\UserController@index');
+  Route::get(Helper::getRouteWebPath('/admin/users'),'Admin\UserController@index');
   Route::get('/admin/users/list','Admin\UserController@listUser');
   Route::post('/admin/users/create','Admin\UserController@create');
   Route::post('/admin/users/update','Admin\UserController@update');
